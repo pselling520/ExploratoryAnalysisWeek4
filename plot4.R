2062284cec272019c230166d5fa1254ddf77e6b8
Paul Selling <pselling520@gmail.com>
2020-06-22 22:15:40 -0500

    Plot 4 commit

---
+++ plot4.R
@@ -0,0 +1,17 @@
+#Loading in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#grabbing coal data
+coal <- SCC[grep("Coal",SCC$EI.Sector),]
+
+#merging datasets
+combined <- merge(NEI,coal,by="SCC")
+
+g <- ggplot(data=combined, aes(x=as.factor(year),y=Emissions))
+g <- g + geom_col(color="green",fill="green") +
+    labs(x="Year",
+         y="Total Emissions",
+         title="Trending U.S. Coal Emissions")
+
+ggsave(file="plot4.png",g)
