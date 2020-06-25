6ba313bbc03a8aca6a63c59f44d5295977960be7
Paul Selling <pselling520@gmail.com>
2020-06-24 21:21:02 -0500

    Simplifying plot 2

--- plot2.R
+++ plot2.R
@@ -3,22 +3,16 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 #Getting Baltimore data
+bm <- subset(NEI,fips=="24510")
 bm99 <- subset(NEI, fips=="24510" & year==1999)
 bm08 <- subset(NEI, fips=="24510" & year==2008)
 
-bmEmissions99 <- sum(bm99$Emissions)
-bmEmissions08 <- sum(bm08$Emissions)
+#Converting to sums
+bm <- aggregate(bm$Emissions,by=list(bm$year),FUN=sum)
 
-y <- c(bmEmissions99, bmEmissions08)
-x <- c(1999,2008)
-y0 <- y[1]
-y1 <- y[2]
-x0 <- 1999
-x1 <- 2008
-
-#plotting 
+#plotting
 dev.new(width=480,height=480,unit="px")
-plot(x,y, main = "Changes in Baltimore Emissions",xlab="Year",ylab="Total Emissions")
-segments(x0,y0,x1,y1)
+plot(bm$Group.1,bm$x, main = "Changes in Baltimore Emissions",xlab="Year",ylab="Total Emissions")
+lines(bm$Group.1,bm$x)
 dev.copy(png, file="plot2.png")
 dev.off()
